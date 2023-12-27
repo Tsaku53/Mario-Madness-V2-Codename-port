861adcbf874023d0356a4c64575067e9c8820fc9
APurples <tylersanders0714@gmail.com>
2023-12-27 13:14:50 -0600

    window shit

---
+++ data/global.hx
@@ -0,0 +1,13 @@
+static var redirectStates:Map<FlxState, String> = [
+    TitleState => "customStates/MarioTitleState",
+];
+
+function update() if (FlxG.keys.justPressed.F5 && FlxG.save.data.devMode) FlxG.resetState();
+
+function preStateSwitch() {
+    FlxG.camera.bgColor = 0xFF000000;
+
+	for (redirectState in redirectStates.keys())
+		if (FlxG.game._requestedState is redirectState)
+            FlxG.game._requestedState = new ModState(redirectStates.get(redirectState));
+}
---
+++ data/states/customStates/MarioTitleState.hx
@@ -0,0 +1,24 @@
+FlxG.resizeWindow(640, 360);
+FlxG.resizeGame(640, 360);
+FlxG.scaleMode.width = 640;
+FlxG.scaleMode.height = 360;
+
+function create(){
+    txtNum = new FlxText(0, 0, 0, "Press Enter to Begin", 32);
+    txtNum.screenCenter();
+    add(txtNum);
+}
+
+function update(){
+    if (controls.ACCEPT){
+        FlxTween.tween(FlxG.camera, {alpha: 0}, .75, {ease: FlxEase.quintInOut});
+        new FlxTimer().start(1, function(tmr:FlxTimer){
+            FlxTween.tween(window, {width: 1280, height: 720}, 1, {ease: FlxEase.quartOut}).onComplete = function(){
+                FlxG.resizeWindow(1280, 720);
+                FlxG.scaleMode.width = 1280;
+                FlxG.scaleMode.height = 720;
+                FlxG.switchState(new MainMenuState());
+            };
+        });
+    }
+}
