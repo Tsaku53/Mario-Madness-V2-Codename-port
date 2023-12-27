14ad4ab76389e1df7e2ab2ff5581da9f04deac3d
APurples <tylersanders0714@gmail.com>
2023-12-27 14:09:14 -0600

    titlestate plays music now

--- data/states/customStates/MarioTitleState.hx
+++ data/states/customStates/MarioTitleState.hx
@@ -3,6 +3,8 @@ FlxG.resizeGame(640, 360);
 FlxG.scaleMode.width = 640;
 FlxG.scaleMode.height = 360;
 
+function new() CoolUtil.playMenuSong();
+
 function create(){
     enterTxt = new FlxText(0, 0, 0, "Press Enter to Begin", 32);
     enterTxt.screenCenter();
@@ -11,8 +13,10 @@ function create(){
 
 function update(){
     if (controls.ACCEPT){
-        FlxTween.tween(FlxG.camera, {alpha: 0}, .75, {ease: FlxEase.quintInOut});
-        new FlxTimer().start(1, function(tmr:FlxTimer){
+        FlxG.camera.flash(FlxColor.WHITE, 1);
+		CoolUtil.playMenuSFX(1);
+        new FlxTimer().start(.75, function(tmr:FlxTimer){FlxTween.tween(FlxG.camera, {alpha: 0}, .75, {ease: FlxEase.quintInOut});});
+        new FlxTimer().start(1.5, function(tmr:FlxTimer){
             FlxTween.tween(window, {width: 1280, height: 720}, 1, {ease: FlxEase.quartOut}).onComplete = function(){
                 FlxG.resizeWindow(1280, 720);
                 FlxG.scaleMode.width = 1280;
