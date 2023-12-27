2e0e5c47981adee7d48b29e7f05443233a7660e8
Tsaku <85741930+Tsaku53@users.noreply.github.com>
2023-12-27 19:23:19 +0100

    update flash thing

--- data/stages/nesbeat.hx
+++ data/stages/nesbeat.hx
@@ -1,30 +1,46 @@
-function postUpdate(elapsed:Float) {
-	switch(curCameraTarget) {
-		case 0: 	dad.visible = true;
-					boyfriend.visible = false;
-					gf.visible = false;
-					blackBarThingie.alpha = 0;
 
-					alreadychange = false;
-
-		case 1:
-					dad.visible = false;
-					boyfriend.alpha = 1;
-					boyfriend.visible = true;
-					gf.visible = true;
-					gf.alpha = 1;
-					blackBarThingie.alpha = 0.3;
-
-	}
-}
-
-function postCreate() {
-    var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('stages/beatus/bars'));
-	bg.scale.set(3, 3);
+var blackBarThingie:FlxSprite;
+function create() {
 	bg.screenCenter();
-	bg.scrollFactor.set(0, 0);
-	bg.antialiasing = true;
-	insert(members.indexOf(dad) - 1, bg);
-	add(bg);
 
+	bg.antialiasing = true;
+	add(bg);
+	FlxTween.tween(bg, {angle: 360}, 40, {ease: FlxEase.smootherStepInOut, type: FlxTween.PINGPONG});
+
+    	blackBarThingie = new FlxSprite().makeSolid(FlxG.width, FlxG.height, FlxColor.BLACK);
+	blackBarThingie.setGraphicSize(Std.int(blackBarThingie.width + 100));
+	blackBarThingie.scrollFactor.set(0, 0);
+	blackBarThingie.screenCenter();
+	add(blackBarThingie);
 }
+
+var dummyvar = 0;
+function postUpdate(elapsed:Float) {
+    switch(curCameraTarget) {
+        case 0:     dad.visible = true;
+                    boyfriend.visible = false;
+                    gf.visible = false;
+                    
+            if (dummyvar != -1) {
+			blackBarThingie.alpha = 0.3;
+                        FlxTween.tween(blackBarThingie, {alpha: 0},1, {ease: FlxEase.linear, type: FlxTween.ONESHOT});    
+            		dummyvar = -1;
+            }
+
+                    alreadychange = false;
+
+        case 1:
+                    dad.visible = false;
+                    boyfriend.alpha = 1;
+                    boyfriend.visible = true;
+                    gf.visible = true;
+                    gf.alpha = 1;
+                    
+            if (dummyvar != 1) {
+			blackBarThingie.alpha = 0.3;
+                        FlxTween.tween(blackBarThingie, {alpha: 0},1, {ease: FlxEase.linear, type: FlxTween.ONESHOT});    
+                        dummyvar = 1;
+            }
+    }
+}
+
